73082a8e9139bd040b16eeee89080f3b21194769
w66236 <115528787+w66236@users.noreply.github.com>
2022-11-08 09:14:52 +0100

    Create lab4.java

---
+++ lab4.java
@@ -0,0 +1,44 @@
+package wsiz.Klasy;
+
+public class KlasyLAB4 {
+//    double dlugosc, szerokosc;
+//
+//    public Prostokat(double dlugosc, double szerokosc) {
+//        this.dlugosc = dlugosc;
+//        this.szerokosc = szerokosc;
+//    }
+//    public double Pole() {
+//        return dlugosc*szerokosc;
+//    }
+//
+//    public  double Odwod(){
+//        return (2*dlugosc)+(2*szerokosc);
+//    }
+//    public  double Przekatna(){
+//        return Math.sqrt(((Math.pow(dlugosc,2))+Math.pow(szerokosc,2)));
+//    }
+//
+//    public void Wypisz(){
+//        System.out.println("Prostokat o boku a: "+dlugosc+ "i boku b = "+szerokosc);
+//        System.out.println("Pole wynosi: "+Pole());
+//        System.out.println("Obwod wynosi: "+Odwod());
+//        System.out.println("Przekatna: "+Przekatna());
+//    }
+
+//    String nazwa;
+//    int lpPieter;
+//    LocalDate rokBudowy;
+//
+//    public  Budynek(String nazwa, int lgPieter, LocalDate rokBudowy) {
+//        this.nazwa = nazwa;
+//        this.lpPieter = lpPieter;
+//        this.rokBudowy = rokBudowy;
+//
+//    }
+//
+//    public void setNazwa(String nazwa) { this.nazwa = nazwa;}
+
+
+    
+
+}
